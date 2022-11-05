7fb526b7399aa26ecfeb553d98408180f5323e6e
OKiseleva1 <kiseleva2301@yandex.ru>
2022-11-05 10:07:49 +0300

    week28 Выбор тарифа

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import styles from './App.module.css';
 import Rate from './rates/rate';
-
-const rates = [
-  { name: "Безлимитный 300", price: "300 руб/мес", speed: "до 10 Мбит/сек", text: "Объем включенного трафика не ограничен" },
-  { name: "Безлимитный 450", price: "450 руб/мес", speed: "до 50 Мбит/сек", text: "Объем включенного трафика не ограничен" },
-  { name: "Безлимитный 550", price: "550 руб/мес", speed: "до 100 Мбит/сек", text: "Объем включенного трафика не ограничен", isSelected: true },
-  { name: "Безлимитный 1000", price: "1000 руб/мес", speed: "до 200 Мбит/сек", text: "Объем включенного трафика не ограничен" }
-]
+import { useState } from 'react';
 
 
 function App() {
+
+  const [isSelect, setIsSelect] = useState(0);
   return (
     <div className={styles.App}>
-      {
-        rates.map((rate) =>
-          <Rate name={rate.name} price={rate.price} speed={rate.speed} text={rate.text} isSelected={rate.isSelected} />
-        )
-      }
+      <Rate price="300" speed="до 10 Мбит/сек" color="1" isSelect={isSelect === "1"} onSetSelect={setIsSelect} />
+      <Rate price="450" speed="до 50 Мбит/сек" color="2" isSelect={isSelect === "2"} onSetSelect={setIsSelect} />
+      <Rate price="550" speed="до 100 Мбит/сек" color="3" isSelect={isSelect === "3"} onSetSelect={setIsSelect} />
+      <Rate price="1000" speed="до 200 Мбит/сек" color="4" isSelect={isSelect === "4"} onSetSelect={setIsSelect} />
     </div>
   );
 }
--- src/rates/rate.jsx
+++ src/rates/rate.jsx
@@ -1,18 +1,32 @@
-import styles from './Rate.module.css';
+import { useState } from 'react';
+import "./rate.css"
 
 
 
-function Rate(props) {
+export default function Rate(props) {
+
+    const { color, isSelect, onSetSelect } = props;
+
+
+    let rateSelected = 'rate';
+
+    if (isSelect) {
+        rateSelected = 'selected';
+    }
+
+    function onClick() {
+        onSetSelect(color);
+    }
+
     return (
 
-        <div className={props.isSelected ? styles.selected : ""}>
-            <div className={styles.cardName}>{props.name}</div>
-            <div className={styles.cardPrice}>{props.price}</div>
-            <div className={styles.cardSpeed}>{props.speed}</div>
-            <div className={styles.cardText}>{props.text}</div>
+        <div className={rateSelected} onClick={onClick}>
+            <div className="cardName">Безлимитный {props.price}</div>
+            <div className="cardPrice">{props.price} руб/мес</div>
+            <div className="cardSpeed">{props.speed}</div>
+            <div className="cardText">Объем включенного трафика не ограничен</div>
         </div >
     );
 }
 
 
-export default Rate;
